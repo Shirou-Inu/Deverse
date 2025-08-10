d87c2f88eb04b9877ff62e5d89fec5e40c8963e6
Shirou-Inu <aaronyang652@gmail.com>
2025-08-10 17:50:23 -0400

    Add animated sidebar with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useRoutes } from "react-router-dom"
-import routes from "./common/utils/routes"
+import { useRoutes } from "react-router-dom";
+import routes from "./common/utils/routes";
+import Sidebar from "./common/components/Sidebar/Sidebar";
 
 function App() {
-    const routing = useRoutes(routes)
-    return (
-        <div>
-            <div>
-                {routing}
-            </div>
-        </div>
-    )
+  const routing = useRoutes(routes);
+  return (
+    <main className="flex min-h-screen">
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <h1>{routing}</h1>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/common/utils/routes.tsx
+++ src/common/utils/routes.tsx
@@ -1,10 +1,14 @@
-import { type RouteObject } from "react-router-dom"
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage"
-import Home from "../../pages/Home/Home"
+import { type RouteObject } from "react-router-dom";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import Home from "../../pages/Home/Home";
+import About from "../../pages/About/About";
+import Template from "../../pages/Template/Template";
 
 const routes: RouteObject[] = [
-  {path: "*", element: <NotFoundPage/>},
-  {path: "/", element: <Home/>},
-]
+  { path: "*", element: <NotFoundPage /> },
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/template", element: <Template /> },
+];
 
-export default routes
+export default routes;
