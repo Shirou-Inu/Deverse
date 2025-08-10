3da92773bca3ba10fdaacec074aef491655d45f6
Shirou-Inu <aaronyang652@gmail.com>
2025-08-10 17:13:41 -0400

    Add default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-dark': 'var(--bg-dark)',
+        'bg': 'var(--bg)',
+        'bg-light': 'var(--bg-light)',
+        'text': 'var(--text)',
+        'text-muted': 'var(--text-muted)',
+        'highlight': 'var(--highlight)',
+        'border': 'var(--border)',
+        'border-muted': 'var(--border-muted)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'danger': 'var(--danger)',
+        'warning': 'var(--warning)',
+        'success': 'var(--success)',
+        'info': 'var(--info)'
+      }
+    },
   },
   plugins: [],
 }
