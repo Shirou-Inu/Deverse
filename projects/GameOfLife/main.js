c5a37fe0d8af2098a37103f9c5b8bfc17b063bc6
Shirou-Inu <aaronyang652@gmail.com>
2025-09-04 20:10:53 -0400

    Updates

--- projects/GameOfLife/main.js
+++ projects/GameOfLife/main.js
@@ -10,6 +10,7 @@ let height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 // Variables
+let margin = 20;
 // Handle window resize
 window.addEventListener("resize", () => {
     width = window.innerWidth;
@@ -17,7 +18,7 @@ window.addEventListener("resize", () => {
     canvas.width = width;
     canvas.height = height;
 });
-const grid = new Grid(10, 10, 250, 500);
+const grid = new Grid(10, 10, width - 2 * margin, (0.75 * height) - 2 * margin);
 grid.initGrid(0.25);
 function draw() {
     // Clear screen
@@ -25,7 +26,13 @@ function draw() {
     // Draw background
     ctx.fillStyle = "Gray";
     ctx.fillRect(0, 0, width, height);
+    // Translate origin
+    ctx.save();
+    ctx.translate(margin, 0.25 * height + margin);
+    // Draw grid
     grid.draw(ctx);
+    // Restore origin
+    ctx.restore();
     // Redraw
     requestAnimationFrame(draw);
 }
